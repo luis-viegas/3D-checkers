345edd6c802cd0dfee5725d66b24928dc3f2bd59
Diogo Maia <up201904974@fe.up.pt>
2022-09-30 21:43:22 +0100

    DemoXML

--- tp1/MySceneGraph.js
+++ tp1/MySceneGraph.js
@@ -900,6 +900,6 @@ export class MySceneGraph {
     //TODO: Create display loop for transversing the scene graph
 
     //To test the parsing/creation of the primitives, call the display function directly
-    this.primitives["demoCylinder"].display();
+    this.primitives["demoRectangle"].display();
   }
 }
